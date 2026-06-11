7751224296d6cc57898fe02dffa8108ba2964a6c
Raja Bhaiya <Rajabhaiya.niranjan@harman.com>
2026-06-11 17:51:14 -0400

    Update Core AI chat app visual assets

--- Examples/02-CoreAI-LocalChat/CoreAIChat/CoreAIChat/Shared/DesignSystem/AppColors.swift
+++ Examples/02-CoreAI-LocalChat/CoreAIChat/CoreAIChat/Shared/DesignSystem/AppColors.swift
@@ -1,15 +1,63 @@
 import SwiftUI
 
+#if canImport(UIKit)
+import UIKit
+#elseif canImport(AppKit)
+import AppKit
+#endif
+
 enum AppColors {
-    static let background = Color(.systemGroupedBackground)
-    static let surface = Color(.secondarySystemBackground)
-    static let elevatedSurface = Color(.systemBackground)
-    static let groupedBackground = Color(.systemGroupedBackground)
+    static let background = platformBackground
+    static let surface = platformSurface
+    static let elevatedSurface = platformElevatedSurface
+    static let groupedBackground = platformBackground
     static let userBubble = Color.accentColor
-    static let assistantBubble = Color(.secondarySystemGroupedBackground)
-    static let separator = Color(.separator)
+    static let assistantBubble = platformAssistantBubble
+    static let separator = platformSeparator
     static let success = Color.green
     static let warning = Color.orange
     static let danger = Color.red
     static let neutral = Color.secondary
+}
+
+private extension AppColors {
+    static let platformBackground: Color = {
+        #if canImport(UIKit)
+        Color(uiColor: .systemGroupedBackground)
+        #elseif canImport(AppKit)
+        Color(nsColor: .windowBackgroundColor)
+        #endif
+    }()
+
+    static let platformSurface: Color = {
+        #if canImport(UIKit)
+        Color(uiColor: .secondarySystemBackground)
+        #elseif canImport(AppKit)
+        Color(nsColor: .controlBackgroundColor)
+        #endif
+    }()
+
+    static let platformElevatedSurface: Color = {
+        #if canImport(UIKit)
+        Color(uiColor: .systemBackground)
+        #elseif canImport(AppKit)
+        Color(nsColor: .textBackgroundColor)
+        #endif
+    }()
+
+    static let platformAssistantBubble: Color = {
+        #if canImport(UIKit)
+        Color(uiColor: .secondarySystemGroupedBackground)
+        #elseif canImport(AppKit)
+        Color(nsColor: .unemphasizedSelectedContentBackgroundColor)
+        #endif
+    }()
+
+    static let platformSeparator: Color = {
+        #if canImport(UIKit)
+        Color(uiColor: .separator)
+        #elseif canImport(AppKit)
+        Color(nsColor: .separatorColor)
+        #endif
+    }()
 }
